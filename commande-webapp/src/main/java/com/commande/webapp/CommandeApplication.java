bd111a6ef926014e38b91c6572769cc906fc27b8
Lova Mialy <ramalomi@gmail.com>
2024-10-25 22:03:16 -0400

    generation des entités

---
+++ commande-webapp/src/main/java/com/commande/webapp/CommandeApplication.java
@@ -0,0 +1,11 @@
+package com.commande.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommandeApplication.class, args);
+    }
+}
